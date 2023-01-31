53ba9b6feece476e1019b1a6392fdf0a9cb765ed
Mac_Developer <araozangel842@gmail.com>
2023-01-31 10:56:25 +0200

    Modify Employee oject

--- lib/crud/employee.dart
+++ lib/crud/employee.dart
@@ -1,3 +1,5 @@
+Employee toEmployee(Map<String, Object?> map) => Employee.fromMap(map);
+
 class Employee {
   final int id;
   final String name;
@@ -12,4 +14,24 @@ class Employee {
     required this.designation,
     required this.isMale,
   });
+
+  // Convert a Employee object into a Map object
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'designation': designation,
+      'isMale': isMale,
+    };
+  }
+
+  // Convert a Map object into a Employee object
+  factory Employee.fromMap(Map<String, dynamic> map) => Employee(
+        id: map['id'],
+        name: map['name'],
+        email: map['email'],
+        designation: map['designation'],
+        isMale: map['isMale'],
+      );
 }
